97ce0172dfef36b31519f070a855fdec34188437
balanasia <anabalasia@gmail.com>
2022-10-10 08:24:27 -0500

    Finish String Methods and Type Casting

---
+++ string-methods.py
@@ -0,0 +1,22 @@
+name = "hunter"
+
+#length if the string
+print(len(name))
+#find index of the string
+print(name.find("t"))
+#capitalize
+print(name.capitalize())
+#uppercase
+print(name.upper())
+#lowercase
+print(name.lower())
+#isDigit
+print(name.isdigit())
+#are these alphabetical characters?
+print(name.isalpha())
+#count how many characters in the string
+print(name.count("u"))
+#replace character in a string
+print(name.replace("h", "b"))
+#display string n times
+print(name*3)
---
+++ typecasting.py
@@ -0,0 +1,18 @@
+#typecasting  = convert the data typeof a value to anotherdata type
+import string
+
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #string
+
+#convert x to float
+x = float(x)
+#turn y into int
+y = str(y)
+#turn string into int
+z = int(z)
+
+print(x)
+print("Y is " + y)
+print(z*3)
