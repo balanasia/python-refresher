25367ffa0454d114509bdaaf270677d65348f92e
balanasia <anabalasia@gmail.com>
2022-10-18 14:32:16 -0500

    Finish Inheritence

--- Animal.py
+++ Animal.py
@@ -2,17 +2,26 @@ class Animal():
 
     alive = True
 
-    def eat():
+    def eat(self):
         print("This animal is eating")
 
-    def sleep():
+    def sleep(self):
         print("This animal is sleeping")
 
 class Rabbit(Animal):
-   pass
+    def run(self):
+        print("This rabbit is running")
 
 class Hawk(Animal):
-    pass
+    def fly(self):
+        print("This hawk is flying")
 
 class Fish(Animal):
     pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+rabbit.eat()
+hawk.fly()
