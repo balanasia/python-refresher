7992935428f585cb3c0e0eb8a5c2b77b382cb660
balanasia <anabalasia@gmail.com>
2022-10-25 04:26:42 -0500

    Finish Map, Filter and Reduce functions

---
+++ map-function.py
@@ -0,0 +1,23 @@
+# map() = applies a function to each item in an iterable (lists,tuple, etc)
+# 
+# map(function,iterable)
+
+store = [("shirt", 20.00),
+         ("pants", 25.00),
+         ("jacket", 50.00),
+         ("socks", 10.00)
+]
+
+to_euros = lambda data:(data[0], data[1]*0.82)
+store_euros = list(map(to_euros, store))
+
+to_dollars = lambda data:(data[0], data[1]/0.82)
+store_dollars = list(map(to_dollars, store))
+
+print("In dollars:")
+for i in store_dollars:
+    print(i)
+
+print("In euros:")
+for i in store_euros:
+    print(i)
